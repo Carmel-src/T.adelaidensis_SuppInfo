f17fb4f25734ffbc7ef3b93dc36ae973c5deb8d6
Carmel <34261547+Carmel-src@users.noreply.github.com>
2022-10-21 11:37:18 +1030

    Add files via upload

--- Chapter 5 - Gene Expression/kallisto-clstr-clean.sh
+++ Chapter 5 - Gene Expression/kallisto-clstr-clean.sh
@@ -6,56 +6,25 @@
 # The paths in the script assume that a specific directory structure has been set up.
 # Kallisto must be run on paired samples separately
 # 
-# usage on NECTAR
-# usage from within: conda activate anaKallisto
-# usage from /mnt/DE-analysis/bash
-# activate screen
-# bash kallisto-clstr-clean.sh &> /mnt/DE-analysis/2_alignedData/kallisto-clstr-clean/kallisto-clstr-clean-log.txt
+# usage on flinder's Deep Thought Machine via SLURM
+# usage from within: conda activate ShortConda
 #
 # Carmel Maher & Terry Bertozzi
-# Dec 2019, Altered Sep, Dec 2020. 12/1/21
+# Dec 2019, last altered Oct 2022
 
 
 #------adjust these for your run-----
 
-# Miniconda3 4.7.12
-#(changed PYTHONPATH from Cogent install -> also this is Python Python 3.7.4, that was Python 2.7)
-# conda activate anaKallisto
-# conda install -c bioconda kallisto
-# kallisto, version 0.46.1
+# module load Miniconda3/4.9.2
+# conda activate ShortConda
+# conda install kallisto
+# kallisto, version 0.48.1
 
 
-# ReferenceDIR=/mnt/IsoSeq-analysis/data/Seqtk #contains reference_transcripts.1Lvclean.fasta
+# ReferenceDIR=/scratch/user/mahe0050/IsoSeq-analysis/data/Seqtk #contains reference_transcripts.1Lvclean.fasta
 # KallistoEXE=/mnt/Prog/miniconda3/envs/anaKallisto/bin
-KallistoDIR=/mnt/DE-analysis/2_alignedData/kallisto-clstr-clean
-ReadDIR=/mnt/DE-analysis/1_trimmedData #contains trimmed R1, R2, and singletons
-
-#------------------------------------
-
-# *** make the index: (done manually before script) ***
-
-# cd /mnt/DE-analysis/2_alignedData
-# mkdir kallisto-clstr-clean
-# cd /mnt/DE-analysis/2_alignedData/kallisto-clstr-clean
-
-#create the index
-#the below contains the representative transcript for clustered isoforms with (most) of the poly-a tails trimmed:
-# kallisto index -i reference_transcripts.1Lv.clean.idx /mnt/IsoSeq-analysis/data/Seqtk/reference_transcripts.1Lv.clean.fasta
-
-
-# ______
-
-# [build] loading fasta file /mnt/IsoSeq-analysis/data/Seqtk/reference_transcripts.1Lv.clean.fasta
-# [build] k-mer length: 31
-# [build] counting k-mers ... done.
-# [build] building target de Bruijn graph ...  done
-# [build] creating equivalence classes ...  done
-# [build] target de Bruijn graph has 59186 contigs and contains 17770125 k-mers
-
-# Worked better than kallisto-clstr-trim_test because this .idx did not need --make-unique to replace repeated names with unique names
-
-# ______
-
+KallistoDIR=/scratch/user/mahe0050/DE-analysis/2_alignedData/kallisto-clstr-cleanq
+ReadDIR=/scratch/user/mahe0050/DE-analysis/1_trimmedData/q #contains trimmed R1, R2
 
 #------------------------------------
 
@@ -69,10 +38,29 @@ function error_exit
     exit 1
 }
 
+# *** make the index: ***
+ cd /scratch/user/mahe0050/DE-analysis/2_alignedData/kallisto-clstr-cleanq
+
+#create the index
+#the below contains the representative transcript for clustered isoforms with (most) of the poly-a tails trimmed:
+# kallisto index -i reference_transcripts.1Lv.clean.idx /scratch/user/mahe0050/IsoSeq-analysis/data/Seqtk/reference_transcripts.1Lv.clean.fasta || error_exit "$LINENO: kallisto index error"
+
+# ------------
+
+## [build] loading fasta file /scratch/user/mahe0050/IsoSeq-analysis/data/Seqtk/reference_transcripts.1Lv.clean.fasta
+## [build] k-mer length: 31
+## [build] counting k-mers ... done.
+## [build] building target de Bruijn graph ...  done 
+## [build] creating equivalence classes ...  done
+## [build] target de Bruijn graph has 59186 contigs and contains 17770125 k-mers 
+
+# ------------
+
+
 # need to be in the input directory for for file
 cd $ReadDIR
 
-for file in *R1_clean.fq.gz
+for file in *R1_cleanq.fq.gz
 do
 	FILESTEM=${file%_*}
 	#this FILESTEM only cuts to _clean, the _R1 is included
@@ -81,8 +69,9 @@ do
 
 echo $FILESTEM
 
-kallisto quant -i $KallistoDIR/reference_transcripts.1Lv.clean.idx -o $KallistoDIR/$FILESTEM"kallisto-out" -b, --bootstrap-samples=100 --threads=8 --pseudobam $file $FILESTEM"R2_clean.fq.gz" || error_exit "$LINENO: kallisto error at $FILESTEM"
+kallisto quant -i $KallistoDIR/reference_transcripts.1Lv.clean.idx -o $KallistoDIR/$FILESTEM"kallisto-out" -b, --bootstrap-samples=100 --threads=12 --pseudobam $file $FILESTEM"R2_cleanq.fq.gz" || error_exit "$LINENO: kallisto error at $FILESTEM"
 
 
 done
-echo "kallisto-clstr-clean done"
+
+echo "kallisto-clstr-cleanq done"
